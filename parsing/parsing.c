066a9a795ec25807b39fc2ac0569d6bd2a943e8f
Ryadhmz <109631638+Ryadhmz@users.noreply.github.com>
2023-04-03 18:39:30 +0200

    Check quotes bon go parser les arguments 2

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: rhamza <rhamza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 00:19:13 by rhamza            #+#    #+#             */
-/*   Updated: 2023/04/03 18:22:47 by rhamza           ###   ########.fr       */
+/*   Updated: 2023/04/03 18:29:51 by rhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 // check si le nb de guillements et bon puis mettre tt les arguments dans une liste chainee arg
 
-static int ft_quotes(char *str) // si impair quotes pas bon sauf si dans autres types de quotes 
+static int ft_quotes(char *str, int simple_quote, int double_quote) // si impair quotes pas bon sauf si dans autres types de quotes 
 {
-    int simple_quote;
-    int double_quote;
-
-    simple_quote = 0;
-    double_quote = 0;
+    int if_quote;
+    
+    if_quote = 0;
     while(str[0] != '\0')
     {
         if(str[0] == 39 && simple_quote < 2 && double_quote == 0) // simple quote
@@ -28,9 +26,15 @@ static int ft_quotes(char *str) // si impair quotes pas bon sauf si dans autres 
         else if(str[0] == 34 && double_quote < 2 && simple_quote == 0) // double quote
             double_quote += 1;
         if(simple_quote == 2)
+        {
+            if_quote = 1;
             simple_quote = 0;
+        }
         if(double_quote == 2)
+        {
+            if_quote = 1;
             double_quote = 0;
+        }
         str += 1;
     }
     if((simple_quote % 2 == 0 || simple_quote == 0) && (double_quote % 2 == 0 || double_quote == 0))
@@ -40,7 +44,6 @@ static int ft_quotes(char *str) // si impair quotes pas bon sauf si dans autres 
 #include <stdio.h>
 void parsing(char *str)
 {
-    // char **parsed;
-    printf("%d\n", ft_quotes(str));
+    printf("%d\n", ft_quotes(str, 0, 0));
     // parsed = ft_split_boost(str);
 }
